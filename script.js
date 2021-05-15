b430c9b9633aefe3523d47840c5bd3cb7da47411
Joao-santos-oliveira <80595573+Joao-santos-oliveira@users.noreply.github.com>
2021-05-15 18:40:43 -0300

    soma/media

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = prompt ('Insira primeiro numero');
+let b = prompt ('Insira segundo numero');
+let c = prompt ('Insira terceiro numero');
+let d = prompt ('Insira quarto numero');
+
+let soma = parseInt(a) + parseInt(b) + parseInt(c) + parseInt(d)
+let media = soma / 4
+alert(`Media: ${media}\n Soma Total: ${soma}`)
